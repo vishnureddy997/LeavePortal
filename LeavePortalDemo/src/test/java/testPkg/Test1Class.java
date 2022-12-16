c0ca4ee7790cab9703766a0766600f01d7b16cd0
vishnureddy997 <57907899+vishnureddy997@users.noreply.github.com>
2022-12-16 18:30:39 +0530

    Add files via upload

---
+++ LeavePortalDemo/src/test/java/testPkg/Test1Class.java
@@ -0,0 +1,29 @@
+package testPkg;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test1Class 
+{ 
+//	@Test
+//	
+//	public static void main(String[] args) {
+//	System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\Downloads\\chromedriver.exe");
+//	WebDriver driver = new ChromeDriver();
+//	driver.get("https://attom:d91f0fc6@rtweb-qa.azurewebsites.net/");
+//	}
+//}
+
+@Test
+
+//public static void main(String[] args)
+
+	public void LaunchBrowser() {
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\Downloads\\chromedriver.exe");
+WebDriver driver = new ChromeDriver();
+driver.get("http://192.168.137.2:7895/LeavePortal/Login/True");
+driver.manage().window().maximize();
+}
+}
