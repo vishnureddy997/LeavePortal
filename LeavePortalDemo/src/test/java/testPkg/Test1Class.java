53bdf1a4f619b899daa8b05a69a10a693cc0bcd3
vishnureddy997 <57907899+vishnureddy997@users.noreply.github.com>
2022-12-28 15:59:39 +0530

    Update Test1Class.java

--- LeavePortalDemo/src/test/java/testPkg/Test1Class.java
+++ LeavePortalDemo/src/test/java/testPkg/Test1Class.java
@@ -15,15 +15,12 @@ public class Test1Class
 //	driver.get("https://attom:d91f0fc6@rtweb-qa.azurewebsites.net/");
 //	}
 //}
-WebDriver driver;
+
 @Test
 
 //public static void main(String[] args)
 
 	public void LaunchBrowser() {
-		WebDriverManager.chromedriver().setup();//opening the chrome driver
-
-		driver=new ChromeDriver();
 System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\Downloads\\chromedriver.exe");
 WebDriver driver = new ChromeDriver();
 driver.get("http://192.168.137.2:7895/LeavePortal/Login/True");
