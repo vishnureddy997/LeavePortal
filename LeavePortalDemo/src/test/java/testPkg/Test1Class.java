95b0dc1bd4134637873374a0fd624fb3a4ee6549
vishnureddy997 <57907899+vishnureddy997@users.noreply.github.com>
2022-12-29 18:42:45 +0530

    Update Test1Class.java

--- LeavePortalDemo/src/test/java/testPkg/Test1Class.java
+++ LeavePortalDemo/src/test/java/testPkg/Test1Class.java
@@ -7,20 +7,7 @@ import org.testng.annotations.Test;
 
 public class Test1Class 
 { 
-//	@Test
-//	
-//	public static void main(String[] args) {
-//	System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\Downloads\\chromedriver.exe");
-//	WebDriver driver = new ChromeDriver();
-//	driver.get("https://attom:d91f0fc6@rtweb-qa.azurewebsites.net/");
-//	}
-//}
-
-@Test
-
-//public static void main(String[] args)
-
-	public void LaunchBrowser() {
+public static void LaunchBrowser() {
 System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 WebDriver driver = new ChromeDriver();
 driver.get("http://113.193.29.70:7895/LeavePortal/CreatePassword?randomPassword=$2a$10$iU4hai2Ja/GlhIoc8EyM.egfkBcysjzMq/UideYZ2YlwsRRhsUgiC");
