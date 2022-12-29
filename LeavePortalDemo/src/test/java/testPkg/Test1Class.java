d1cdc9403e4485e3549bce81e2cabc1872a28557
vishnureddy997 <57907899+vishnureddy997@users.noreply.github.com>
2022-12-29 12:36:27 +0530

    Update Test1Class.java

--- LeavePortalDemo/src/test/java/testPkg/Test1Class.java
+++ LeavePortalDemo/src/test/java/testPkg/Test1Class.java
@@ -1,9 +1,32 @@
 package testPkg;
 
 
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import org.testng.Assert;
+
+import org.testng.annotations.AfterClass;
+
+import org.testng.annotations.BeforeClass;
+
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 
 //public class Test1Class 
 	
