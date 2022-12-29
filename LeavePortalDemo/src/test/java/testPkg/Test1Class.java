ee6058557f73f9a80e53844bd921c7605ea09f6c
vishnureddy997 <57907899+vishnureddy997@users.noreply.github.com>
2022-12-29 10:32:24 +0530

    Update Test1Class.java

--- LeavePortalDemo/src/test/java/testPkg/Test1Class.java
+++ LeavePortalDemo/src/test/java/testPkg/Test1Class.java
@@ -17,13 +17,31 @@ public class Test1Class
 //}
 
 @Test
-
+public void startBrowser(String browserName)
+	{
+		System.out.println("Parameter value is "+browserName);
+		WebDriver driver=null;
+		
+		if(browserName.contains("Chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions opt=new ChromeOptions();
+			opt.addArguments("--headless");
+			opt.addArguments("--no-sandbox");
+			opt.addArguments("--disable-dev-shm-usage");
+			driver=new ChromeDriver(opt);
+		}
+		else if(browserName.contains("Edge"))
+		{
+			WebDriverManager.edgedriver().setup();
+			 driver=new EdgeDriver();
+		}
 //public static void main(String[] args)
 
-	public void LaunchBrowser() {
-System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\Downloads\\chromedriver.exe");
-WebDriver driver = new ChromeDriver();
-driver.get("http://192.168.137.2:7895/LeavePortal/Login/True");
+	//public void LaunchBrowser() {
+//System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\Downloads\\chromedriver.exe");
 driver.manage().window().maximize();
+driver.get("http://192.168.137.2:7895/LeavePortal/Login/True");
+driver.quit();
 }
 }
