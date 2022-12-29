6e4efe8333b6a2bc619cfc3d58429378c45caf8b
vishnureddy997 <57907899+vishnureddy997@users.noreply.github.com>
2022-12-29 11:38:25 +0530

    Update Test1Class.java

--- LeavePortalDemo/src/test/java/testPkg/Test1Class.java
+++ LeavePortalDemo/src/test/java/testPkg/Test1Class.java
@@ -16,17 +16,37 @@ public class Test1Class
 //	}
 //}
 
-@Test
+//@Test
 
-//public static void main(String[] args)
+public static void main(String[] args) {
+	
+	
+	System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+       
+        //create object of chrome options
+        ChromeOptions options = new ChromeOptions();
+        
+        //add the headless argument
+        options.addArguments("headless");
+        
+        //pass the options parameter in the Chrome driver declaration
+        WebDriver driver = new ChromeDriver(options);
+        
+        //Navigate to toolsQA site url
+        driver.get("http://113.193.29.70:7895/LeavePortal/CreatePassword?randomPassword=$2a$10$iU4hai2Ja/GlhIoc8EyM.egfkBcysjzMq/UideYZ2YlwsRRhsUgiC");
+        
+        //Print the Title of the Page
+        //System.out.println("Title of the page is -> " + driver.getTitle());
+        
+        //Close the driver
+        driver.close();
+    }
+}
 
-	public void LaunchBrowser() {
-System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
-		ChromeOptions options = new ChromeOptions();
-options.addArgument("headless");
-ChromeDriver driver = new ChromeDriver(options);
-WebDriver driver = new ChromeDriver();
-driver.get("http://113.193.29.70:7895/LeavePortal/CreatePassword?randomPassword=$2a$10$iU4hai2Ja/GlhIoc8EyM.egfkBcysjzMq/UideYZ2YlwsRRhsUgiC");
-driver.manage().window().maximize();
-}
-}
+	//public void LaunchBrowser() {
+//System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+//WebDriver driver = new ChromeDriver();
+//driver.get("http://113.193.29.70:7895/LeavePortal/CreatePassword?randomPassword=$2a$10$iU4hai2Ja/GlhIoc8EyM.egfkBcysjzMq/UideYZ2YlwsRRhsUgiC");
+//driver.manage().window().maximize();
+//}
+//}
