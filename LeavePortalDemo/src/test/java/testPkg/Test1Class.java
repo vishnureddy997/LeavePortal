fa0464a1fde9d6eee046a6f7d81f6dcc3d65379c
vishnureddy997 <57907899+vishnureddy997@users.noreply.github.com>
2022-12-30 11:39:12 +0530

    Update Test1Class.java

--- LeavePortalDemo/src/test/java/testPkg/Test1Class.java
+++ LeavePortalDemo/src/test/java/testPkg/Test1Class.java
@@ -4,6 +4,7 @@ package testPkg;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
 
 public class Test1Class 
 { 
@@ -12,7 +13,13 @@ public static void main(String[] args) {
 System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\Downloads\\chromedriver.exe");
 WebDriver driver = new ChromeDriver();
 driver.get("http://113.193.29.70:7895/LeavePortal/CreatePassword?randomPassword=$2a$10$iU4hai2Ja/GlhIoc8EyM.egfkBcysjzMq/UideYZ2YlwsRRhsUgiC");
+	
+thread.sleep(100);	
+	
 driver.manage().window().maximize();
+	
+thread.sleep(100);	
+	
 }
 }
 
